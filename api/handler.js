124ed222086ff143edab765ee4cedec1c1470b71
merlin04 <Merlin04@users.noreply.github.com>
2021-12-28 10:36:04 -0800

    Mock worker environment on server and use fetch handler from image

--- api/handler.js
+++ api/handler.js
@@ -1,36 +1,102 @@
 import image from "../image";
 
-const extensionContentTypes = {
-    "html": "text/html",
-    "css": "text/css",
-    "js": "application/javascript",
-    "json": "application/json",
-    "mjs": "application/javascript"
+// Set up mocked worker environment
+
+const location = {
+    // The domain the server is serving on
+    href: "https://" + process.env.VERCEL_URL
 };
 
-// Handles all requests to the page
-export default function handler(req, res) {
-    let filename = req.url.slice(1);
-    if (filename === "") {
-        filename = "index.html";
+// Mocked `Response` class
+class Response {
+    constructor(body = null, options = {}) {
+        this.status = typeof options.status === "number" ? options.status : 200;
+        this.ok = this.status >= 200 && this.status < 300;
+        this.statusText = options.statusText || "OK";
+        this.headers = options.headers || {};
+        this.body = body;
     }
-    let fileSrc = image[filename];
+}
 
-    if (filename === "index.html") {
+// Mocked `localforage` instance that pulls from the `image` object
+
+// Define a handler for non-implemented methods
+const unimplemented = () => {
+    throw new Error("Not implemented");
+};
+
+const localforage = {
+    getItem: async key => image[key],
+    setItem: async (key, value) => {
+        image[key] = value;
+    },
+    removeItem: async key => {
+        delete image[key];
+    },
+    clear: async () => {
+        image = {};
+    },
+    length: async () => Object.keys(image).length,
+    key: async index => Object.keys(image)[index],
+    keys: async () => Object.keys(image),
+    iterate: async iteratorCallback => {
+        // Iterate over all value/key pairs in datastore.
+        // `iteratorCallback` is called once for each pair, with the following arguments:
+        // 1. value
+        // 2. key
+        // 3. iterationNumber
+        // `iterate` supports early exit by returning non `undefined` value inside the callback.
+        // Resulting value will be passed to `iterate`'s return value.
+
+        const keys = Object.keys(image);
+        for(let i = 0; i < keys.length; i++) {
+            const key = keys[i];
+            const value = image[key];
+            const result = await iteratorCallback(value, key, i);
+            if(result !== undefined) return result;
+        }
+    },
+    setDriver: unimplemented,
+    config: unimplemented,
+    defineDriver: unimplemented,
+    driver: unimplemented,
+    ready: unimplemented,
+    supports: unimplemented,
+    createInstance: unimplemented,
+    createInstance: unimplemented
+};
+
+// Mocked `fetch` that returns a 404 `Response`
+const fetch = async (..._) => new Response("404 not found", {
+    headers: {
+        "Content-Type": "text/plain"
+    },
+    status: 404,
+    statusText: "Not found"
+});
+
+// Load the worker code from the image (in closure to not redeclare exports)
+const onFetch = (() => {
+    let exports;
+    eval(image["_workerFetch.fragment.js"]);
+    return exports;
+})();
+
+// Handles all requests to the page
+export default async function handler(req, res) {
+    // Run the fetch handler
+    const newReq = { ...req, url: location.href + req.url };
+    const response = await onFetch({
+        request: newReq
+    }, localforage);
+
+    if (req.url === "/" || req.url === "/index.html") {
         // Inject bootstrap script
         const scriptTag = `<script type="module" src="/bootstrap.mjs"></script>`;
-        fileSrc = fileSrc.replace(/<\/body>/, scriptTag + "</body>");
+        response.body = response.body.replace(/<\/body>/, scriptTag + "</body>");
     }
 
-    if (!fileSrc) {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end("404 Not Found");
-        return;
-    }
-
-    const extension = filename.split(".").pop();
-    const contentType = extensionContentTypes[extension] || "text/plain";
-
-    res.writeHead(200, { "Content-Type": contentType });
-    res.end(fileSrc);
+    // Call `res` functions with data from `response`
+    res.writeHead(response.status, response.statusText, response.headers);
+    res.end(response.body);
 }
